e767174d4160646da4fc4b53254eb119dca078f9
Nohaiz <nohaiznazier@gmail.com>
2023-12-12 11:15:35 +0300

    Initial Commit

---
+++ Mobile Programming Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mobile Programming Project
+//
+//  Created by NIggeroni Pepperoni on 12/12/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
